9455391d7d551e8ec8659ab42219684e6be89606
Salman893 <salmanvng@gmail.com>
2021-11-27 13:35:20 +0530

    make it better

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.use(errorRoutes);
 
 mongoose.connect(MONGODB_URI)
     .then(() => {
-        app.listen(3000);
+        app.listen(process.env.PORT || 3000);
     })
     .catch(err => {
         console.log(err);
